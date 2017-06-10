90424f3a1e80f1f86c3d529f67fa39e28e9f9686
Beatriz Rodrigues Natário <beatriz_rodrigues21@hotmail.com>
2017-06-10 00:58:07 -0300

    conexao.php
    Estrutura html da página conexao.php

---
+++ application/views/conexao.php
@@ -0,0 +1,21 @@
+﻿<body>
+        <main class="text-center">
+    <h1>Conecte-se ao seu parceiro</h1><br/><br/>
+    <div class="container">
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="col-md-4">
+            <form method="post" id="formAccConection" action="verificarconecta">
+<p>Já possui um código de parceria? Digite abaixo:</p>
+    <input type="text" name="cd_conecta" id="cd_conecta" size="25" placeholder="Código de conexão de parceria"><br><br>
+    <button type="submit" accesskey="C">Confirmar</button>              
+</form>              
+<script>
+    </script>
+    <p>Ainda não possuem um código? Clique no botão abaixo e gere o seu</p>
+    <button accesskey="G" onclick="window.location.href='codigoconexao'">Gerar Código</button>             
+    </div>
+          <div class="col-md-4"></div>
+    </main>
+  </body>
+</html>
