da9867aa383c0d75a414e19f76fdb1de4d4a8bd8
Beatriz Rodrigues Natário <beatriz_rodrigues21@hotmail.com>
2017-06-10 01:30:09 -0300

    Verificação de usuário
    Criação da função para inserir usuário cadastrado no banco e função para verificar se o usuário está cadastrado

--- application/models/UsuarioDAO.php
+++ application/models/UsuarioDAO.php
@@ -1,1 +1,20 @@
-{\rtf1}
+<?php
+class UsuarioDAO extends CI_Model{
+    
+    public function inserir(Usuario $usr){
+        $this->db->insert("usuario",$usr->toArray());
+    }
+    public function verificar(Usuario $usr){
+        $this->db->select('*');
+        $this->db->from('usuario');
+        $this->db->where('ds_email', $usr->getEmail() );
+        $options = [
+    		'cost' => 11,
+    		'salt' => "salççasdsadsgshfjukluilevtgrbhujiomkilufdgsrt",
+		];
+		$pass = password_hash($usr->getSenha(), PASSWORD_BCRYPT, $options);
+        $this->db->where('ds_email', $usr->getEmail() );
+        $this->db->where('cd_senha', $pass);
+        $query = $this->db->get();
+        return $query->num_rows() == 1;
+    }
