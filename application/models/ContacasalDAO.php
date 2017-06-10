a8d1d904c34ab54d0c16d96a221221e5599f514f
Beatriz Rodrigues Natário <beatriz_rodrigues21@hotmail.com>
2017-06-10 02:19:18 -0300

    Model ContacasalDAO
    Método para buscar o maior cd_contacasal no banco

---
+++ application/models/ContacasalDAO.php
@@ -0,0 +1,20 @@
+﻿<?php
+class ContacasalDAO extends CI_Model{
+    
+    public function inserir(Contacasal $ctc){
+        $this->db->insert("contacasal",$ctc->toArray());
+    }
+    
+    public function pegarMaximo(Contacasal $ctc){
+        $this->db->select_max('cd_contacasal');
+        $this->db->from('contacasal');
+        $query=$this->db->get();
+        return $query->result_array()[0]['cd_contacasal'];
+    }
+    
+}
+
+ 
+ 
+
+?>
