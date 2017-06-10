2a5cb728f857cbd83a0ded51e73ee902f5546d73
Beatriz Rodrigues Natário <beatriz_rodrigues21@hotmail.com>
2017-06-10 03:13:03 -0300

    Alteração dos meses
    Switch case para exibir o nome do mês conforme seu número

--- application/views/principal.php
+++ application/views/principal.php
@@ -26,6 +26,20 @@
         $dia = mdate($diaformat, $time);
         $mes = mdate($mesformat, $time);
         $ano = mdate($anoformat, $time);
+	switch ($mes) {
+          case 01:$mes = 'janeiro';break;
+          case 02:$mes = 'fevereiro';break;
+          case 03:$mes = 'março';break;
+          case 04:$mes = 'abril';break;
+          case 05:$mes = 'maio';break;
+          case 06:$mes = 'junho';break;
+          case 07:$mes = 'julho';break;
+          case 08:$mes = 'agosto';break;
+          case 09:$mes = 'setembro';break;
+          case 10:$mes = 'outubro';break;
+          case 11:$mes = 'novembro';break;
+          case 12:$mes = 'dezembro';break;
+        }
         echo '
             <div class="row evento" id="evento'.$evento['cd_evento'].'">
           <div class="col-sm-2">
