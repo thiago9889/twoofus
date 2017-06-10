2b547723316f656ac8c0abe15602cac5a5dc64ea
Thiago Prado Loureiro <thiago9889@gmail.com>
2017-06-10 01:21:48 -0300

    Array para inserir e verificação de maioridade

--- application/models/Usuario.php
+++ application/models/Usuario.php
@@ -1,4 +1,37 @@
- public function getCodigo(){
+<?php
+class Usuario extends CI_Model{
+    private $cd_usuario, $nm_usuario, $ds_email, $cd_senha, $dt_nascimento, $ic_sexo, $cd_conecta, $cd_contacasal;
+    
+    public function maior18($dt_nascimento){
+        $born = explode('-', date('d-m-Y', $dt_nascimento));
+        $compare = explode('-', date('d-m-Y', empty($compare)? time(): $compare));
+        $age = $compare[2] - $born[2];
+        if($compare[1] > $born[1]) return $age;
+        if($compare[1] == $born[1] && $compare[0] > $born[0]) return $age;
+        return --$age;
+    }
+    
+    public function criar($cd_usuario, $ds_email, $cd_senha, $nm_usuario, $dt_nascimento, $ic_sexo, $cd_conecta){
+        $this->cd_usuario = $cd_usuario;
+        $this->ds_email = $ds_email;
+        $this->cd_senha = $cd_senha;
+        $this->nm_usuario = $nm_usuario;
+        $this->dt_nascimento = $dt_nascimento;
+        $this->ic_sexo = $ic_sexo;
+        $this->cd_conecta = $cd_conecta;
+    }
+    
+    public function verificarcodigoigual(){
+        $this->load->model("UsuarioDAO","usrDAO");
+        return $this->usrDAO->procurarIgual($this);
+    }
+    
+    public function estaAutenticado(){
+        $this->load->model("UsuarioDAO","usrDAO");
+        return $this->usrDAO->verificar($this);
+    }
+    
+    public function getCodigo(){
         return $this->cd_usuario;
     }    
         
@@ -35,3 +68,14 @@
         $this->load->model("UsuarioDAO","usrDAO");
         return $this->usrDAO->pegarMaximo($this);
     }
+    
+    public function toArray(){
+        $this->cd_contacasal = '1';
+        return get_object_vars($this);
+    }
+    
+
+}
+
+?>
+
