5d1ab4baeb61ad06d6f0f9248e86b44ab6ddbcf2
Beatriz Rodrigues Natário <beatriz_rodrigues21@hotmail.com>
2017-06-10 01:19:14 -0300

    Alteração no model
    Funções get da classe Usuario

--- application/models/Usuario.php
+++ application/models/Usuario.php
@@ -1,1 +1,37 @@
-{\rtf1}
+ public function getCodigo(){
+        return $this->cd_usuario;
+    }    
+        
+    public function getEmail(){
+        return $this->ds_email;
+    }
+    
+    public function getSenha(){
+        return $this->cd_senha;
+    }
+    
+    public function getNome(){
+        return $this->nm_usuario;
+    }
+    
+    public function getConecta(){
+        return $this->cd_conecta;
+    }
+    public function getNomeUsuario(){
+        $this->load->model("UsuarioDAO","usrDAO");
+        return $this->usrDAO->pegarNomeUsuario($this);
+    }    
+
+    public function getNomeParceiro(){
+        $this->load->model("UsuarioDAO","usrDAO");
+        return $this->usrDAO->pegarNomeParceiro($this);
+    }    
+    
+    public function getContaCasal(){
+        $this->load->model("UsuarioDAO","usrDAO");
+        return $this->usrDAO->pegarContaCasal($this);
+    }
+    public function getMaxContaCasal(){
+        $this->load->model("UsuarioDAO","usrDAO");
+        return $this->usrDAO->pegarMaximo($this);
+    }
