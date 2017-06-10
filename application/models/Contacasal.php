f5fb7b88ff886337bc7bd50eda90bd03f5576f44
Beatriz Rodrigues Natário <beatriz_rodrigues21@hotmail.com>
2017-06-10 02:03:35 -0300

    Model Contacasal.php
    Método criar e método para puxar o código maximo da função pegarMaximo

---
+++ application/models/Contacasal.php
@@ -0,0 +1,23 @@
+﻿<?php
+
+class Contacasal extends CI_Model{
+    private $cd_contacasal;
+    
+    public function criar($cd_contacasal){
+        $this->cd_contacasal = $cd_contacasal;
+    }
+    
+    public function getMaxContaCasal(){
+        $this->load->model("ContacasalDAO","ctcDAO");
+        return $this->ctcDAO->pegarMaximo($this);
+    }
+    
+    public function toArray(){
+        return get_object_vars($this);
+    }
+    
+
+}
+
+?>
+
